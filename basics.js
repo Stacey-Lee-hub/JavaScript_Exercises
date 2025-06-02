368c4a4b775df0ba94f7f273d1d700528273bd7c
Stacey-Lee Pietersen <staceypietersen2@gmail.com>
2025-06-02 08:33:17 +0200

    variables and string manipulation

---
+++ basics.js
@@ -0,0 +1,9 @@
+let firstName = "Stacey-Lee";
+let lastName = "Pietersen";
+let age = 21;
+
+console.log(`Hello, ${firstName} ${lastName}. You are ${age}`);
+
+let age = 22;
+
+console.log(`Hello, ${firstName} ${lastName}. You are ${age}`);
