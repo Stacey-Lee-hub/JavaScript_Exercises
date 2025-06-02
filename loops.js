3d91de7ea9ab36cd0efa37372ecdfbe804887a80
Stacey-Lee Pietersen <staceypietersen2@gmail.com>
2025-06-02 12:13:23 +0200

    Loops

---
+++ loops.js
@@ -0,0 +1,24 @@
+let numbers = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
+
+let count = 5;
+while (count >= 1) {
+    console.log(count);
+    count--;
+}
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 == 0) {
+        console.log(numbers[i]);
+    };
+}
+
+let sum = 0;
+for (let i = 0; i < numbers.length; i++){
+    sum += numbers[i];
+}
+// if log in inside the loop, it outputs the sum for every number in the array as it adds
+console.log(`Sum of numbers: ${sum}`);
