3276e82e1b35def709d336f91f5f46c2b03ca12e
Stacey-Lee Pietersen <staceypietersen2@gmail.com>
2025-06-02 08:47:53 +0200

    array basics

---
+++ arrays.js
@@ -0,0 +1,12 @@
+let colours = [];
+colours.push("red");
+colours.push("purple");
+colours.push("green");
+
+let numbers = [2, 4, 6, 8, 12];
+
+colours.pop();
+colours.unshift("blue");
+
+console.log(`Length of colours: ${colours.length}`);
+console.log(`Length of numbers: ${numbers.length}`);
